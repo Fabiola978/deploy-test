ecb6561e3af9f7c8351c7480f37257d2a81da5f4
Fabiola978 <fabifabiola978@gmail.com>
2023-10-10 08:10:52 -0300

    scripts update

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 const app= express();
 const PORT =45000;
 
+//-------ROUTER------
 app.get('/api', (req, res) => {
 res.json({
     name: 'Deploy test API',
@@ -12,10 +13,30 @@ res.json({
 });
 
 app.get('/api/data', (req, res) => {
-res.json({
-    {
-        username: 'jrodrigez',
-        email:'jrodri@hotmail.com'
-    }
-})
-})
+    res.json([
+        {
+            name: 'Juan manuel',
+            email: 'jmanuel@gmail.com'
+        },
+        {
+            name: 'Maria Luz',
+            email: 'mariLuz@gmail.com'
+        },
+        {
+            name: 'Gabriel',
+            email: 'gabo123l@gmail.com'
+        }
+    ]);
+    });
+
+
+app.use('*', (req, res) => {
+    res.status(404).json({message: 'Resource not found'});
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on port', PORT);
+    
+});
+
+//como son solicitudes get vamos usar el navegador
